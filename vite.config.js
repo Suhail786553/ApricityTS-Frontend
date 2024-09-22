75f8e98ab1c89b8ab5905f3635f02aed1c39e26d
Suhail786553 <ms5505535@gmail.com>
2024-09-22 15:28:51 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     jsxInject: `import React from 'react'`,
+//   },
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  base: '/', // Make sure this is correct, especially if deploying to a subdirectory
 });
