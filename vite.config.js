e2298da77e59b9076c2ede10aaeb0d012042dda1
Suhail786553 <ms5505535@gmail.com>
2024-10-18 21:15:17 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000 // Ensure this matches your desired port
+  },
   base: '/', // Make sure this is correct, especially if deploying to a subdirectory
 });
